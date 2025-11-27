6b849500c91d523fcd77250a3aab2d7af929d990
Arjunchinnu <arjunchinnu604@gmail.com>
2025-11-27 19:18:39 +0530

    Initial commit of Products api

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import auth from "../middleware/auth.js";
+import role from "../middleware/role.js";
+import {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct
+} from "../controllers/productController.js";
+const router = express.Router();
+
+router.post("/", auth, role(["admin"]), createProduct);
+router.get("/", auth, getProducts);
+router.put("/:id", auth, role(["admin"]), updateProduct);
+router.delete("/:id", auth, role(["admin"]), deleteProduct);
+
+export default router;
